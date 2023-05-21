45c91128239e7bcfaf0a04cee491937de365bf5f
rad731 <viktor.299@mail.ru>
2023-05-21 12:02:38 +0300

    создал файл с решением

---
+++ main.py
@@ -0,0 +1,6 @@
+[200~numlist = list(int(x) for x in "57383502919374282019465759391572910475829238364859572924")
+
+x = int(input())
+firstelementindex = numlist.index(x)
+newlist = tuple(numlist[firstelementindex:(numlist.index(x, firstelementindex+1))+1])
+print(newlist)~
